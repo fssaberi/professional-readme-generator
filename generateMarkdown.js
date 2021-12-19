e3e3b0a413eabfc32285de1c4a32853f87a7cb2f
Fatemeh Brizendine <ssaberi324@yahoo.com>
2021-12-19 12:07:48 -0800

    update license section

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,35 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function getLicenseBadge(badge) {
-  let myBadge = ''
-  if (myBadge === 'Apache') {
+function getLicenseBadge(licenses) {
+  let chosenLicense = licenses.licenses;
+  let myLicense = '';
+
+  if (chosenLicense === 'Apache') {
     return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
   }
 
-  if (myBadge ===  'Boost') {
+  if (chosenLicense ===  'Boost') {
     return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
   }
 
-  if (myBadge === 'Eclipse') {
+  if (chosenLicense === 'Eclipse') {
     return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
   }
 
-  if (myBadge === 'MIT') {
+  if (chosenLicense === 'MIT') {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   }
 
-  if (myBadge === 'Mozilla') {
+  if (chosenLicense === 'Mozilla') {
     return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
   }
+  return myLicense;
+  getLicenseSection(licenses);
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function getLicenseLink(license) {}
+function getLicenseLink(licenses) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function getLicenseSection(license) {
+function getLicenseSection(licenses) {
   return `# ${data.licenses}
   ## License`;
 }
@@ -71,7 +75,7 @@ ${data.test}
 
 ## License
 ${data.licenses}
-${getLicenseBadge(badge)}
+${getLicenseBadge(licenses)}
 
 ## Questions
 ${data.github}
